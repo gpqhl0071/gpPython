103cb66edad4962d23b2061d7b58c3311ede25a9
高鹏 <1110@forex.com.cn>
2018-12-28 18:02:21 +0800

    增加过滤敏感文件

--- workUtil/delMainConfig.py
+++ workUtil/delMainConfig.py
@@ -9,6 +9,10 @@ delList = [
     "logback.xml",
 ]
 
+jarList = [
+    "hutool-all"
+]
+
 for root, dirs, files in os.walk("E:\dx-web-3.8.4-SNAPSHOT-dev"):
     # 过滤敏感文件
     if len(files) > 0:
@@ -19,8 +23,14 @@ for root, dirs, files in os.walk("E:\dx-web-3.8.4-SNAPSHOT-dev"):
     # 遍历lib目录下的jar
     if (len(dirs) > 0):
         for dir in dirs:
+            # 锁定lib目录
             if dir == 'lib':
                 handlePath = root + "/" + dir
+                # 遍历目录下的所有jar
                 for root1, dirs1, files1 in os.walk(handlePath):
                     for file in files1:
-                        print(root1 + ", " + file)
+                        # 保留一下开头的JAR，其他删除
+                        if file.startswith('dx-') or file.startswith('hutool'):
+                            print(root1 + ", " + file)
+                        else:
+                            os.remove(root1 + "/" + file)
