21bdcb9af5c119f3dfde217f87dd4ad8efd13af1
高鹏 <1110@forex.com.cn>
2018-12-28 17:55:33 +0800

    增加过滤敏感文件

--- workUtil/delMainConfig.py
+++ workUtil/delMainConfig.py
@@ -9,10 +9,18 @@ delList = [
     "logback.xml",
 ]
 
-for root, dirs, files in os.walk("D:\IdeaProjectsNew1\dx-web3.8.0"):
-    # print('root:' + root + ' ， dirs size:' + len(dirs).__str__())
-    # print("files = ", files)
+for root, dirs, files in os.walk("E:\dx-web-3.8.4-SNAPSHOT-dev"):
+    # 过滤敏感文件
     if len(files) > 0:
         for file in files:
             if file in delList:
                 print(root + ", " + file)
+
+    # 遍历lib目录下的jar
+    if (len(dirs) > 0):
+        for dir in dirs:
+            if dir == 'lib':
+                handlePath = root + "/" + dir
+                for root1, dirs1, files1 in os.walk(handlePath):
+                    for file in files1:
+                        print(root1 + ", " + file)
